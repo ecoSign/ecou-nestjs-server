ab49424126830fc846bfbfceb876616b3169edba
kimja01 <kimja7045@naver.com>
2022-05-25 23:14:52 +0900

    feat(resource(user), swagger): add user res, swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  setupSwagger(app);
+
+  await app.listen(port);
+  console.log(`listening on port ${port}\n`);
 }
 bootstrap();
---
+++ src/util/swagger.ts
@@ -0,0 +1,19 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+/**
+ * Swagger 세팅
+ *
+ * @param {INestApplication} app
+ */
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('EcoSign API')
+    .setDescription('EcoSign 서버 개발을 위한 API 문서입니다.')
+    .setVersion('1.0')
+    // .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
